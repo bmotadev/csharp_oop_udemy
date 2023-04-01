0069e5ffaa48c20045bad5622dfc8b40a61b4a60
Bruno Mota <bmotadev@gmail.com>
2023-03-31 22:50:02 -0300

    :sparkles: feat: tipos basicos de dados parte 2

--- TiposBasicosDeDadosEmCsharp/Program.cs
+++ TiposBasicosDeDadosEmCsharp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 namespace Course
 {
     class Program
@@ -12,11 +13,41 @@ namespace Course
             long n4 =  2147483647L;
             n1++;
 
+            // parte 2
+            bool completo = false;
+            char genero = 'F';
+            char letra = '\u0041';
+            float n5 = 4.5f;
+            double n6 = 4.5;
+            string nome = "Maria Green";
+            object obj1 = "Alex Brown";
+            object obj2 = 4.5f;
+
+            int valor1 = int.MinValue;
+            int valor2 = int.MaxValue;
+            sbyte valor3 = sbyte.MinValue;
+            decimal valor4 = decimal.MaxValue;
+
             Console.WriteLine(x);
             Console.WriteLine(n1);
             Console.WriteLine(n2);
             Console.WriteLine(n3);
             Console.WriteLine(n4);
+
+            // parte 1
+            Console.WriteLine(completo);
+            Console.WriteLine(genero);
+            Console.WriteLine(letra);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(nome);
+            Console.WriteLine(obj1);
+            Console.WriteLine(obj2);
+
+            Console.WriteLine(valor1);
+            Console.WriteLine(valor2);
+            Console.WriteLine(valor3);
+            Console.WriteLine(valor4);
         }
     }
 }
