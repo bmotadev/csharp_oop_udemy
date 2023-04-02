f98cc9d945db906d1faad49c82691c6dfc41357a
Bruno Mota <bmotadev@gmail.com>
2023-04-02 11:30:15 -0300

    :sparkles: feat: imprimindo e usando cultureinfo

--- Aula18SaidaDeDados/Program.cs
+++ Aula18SaidaDeDados/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char genero = 'F';
+            int idade = 32;
+            double saldo = 10.35784;
+            string nome = "Maria";
+
+            Console.Write("Bom dia");
+            Console.WriteLine("Boa tarde!");
+            Console.WriteLine("Boa noite!");
+            Console.WriteLine("----------------");
+            Console.WriteLine(genero);
+            Console.WriteLine(idade);
+            Console.WriteLine(saldo);
+            Console.WriteLine(nome);
+            Console.WriteLine(saldo.ToString("F2"));
+            Console.WriteLine(saldo.ToString("F4"));
+            Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
